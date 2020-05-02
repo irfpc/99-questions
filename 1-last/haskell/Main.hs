e85061e903deb94a78af07ebe1203c218551a7fc
Mohammad Hasani <thebrodmann@protonmail.com>
2020-05-02 19:41:58 +0430

    refactor(1-last): use doctest

--- 1-last/haskell/Main.hs
+++ 1-last/haskell/Main.hs
@@ -1,46 +1,6 @@
 module Main where
 
-import           Data.Maybe
-import           Prelude                 hiding ( last )
-import           System.Exit
-import           Control.Monad
-import           Data.Monoid
-import           Colourista
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as T
-import           Question
-
-nag :: IO ()
-nag = redMessage "I’m afraid you’re mistaken."
-
-status :: Bool -> Text
-status False = formatWith [red] "✘ "
-status True  = formatWith [green] "✓ "
-
-lambda :: Text
-lambda = formatWith [magenta] "λ "
-
-assert :: Eq a => Show a => Text -> [a] -> Maybe a -> IO Bool
-assert message input expected = do
-  T.putStr $ lambda
-  T.putStrLn message
-  unless isPassed nag
-  T.putStr $ status isPassed
-  print output
-  putStrLn mempty
-  pure isPassed
- where
-  output   = last input
-  isPassed = output == expected
-
-assertions :: [IO Bool]
-assertions =
-  [ assert "last [1, 2, 3, 4]"    [1, 2, 3, 4 :: Int] (Just 4)
-  , assert "last ['x', 'y', 'z']" ['x', 'y', 'z']     (Just 'z')
-  ]
+import           Test.DocTest
 
 main :: IO ()
-main = do
-  All isPassed <- foldMap All <$> sequence assertions
-  unless isPassed exitFailure
+main = doctest ["Question.hs"]
--- 1-last/haskell/Question.hs
+++ 1-last/haskell/Question.hs
@@ -1,9 +1,21 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
--- |
+module Question
+  ( last
+  )
+where
 
-module Question (last) where
+import           Prelude                 hiding ( last )
 
-import Prelude hiding (last)
-
+-- | Find the last element of a list.
+--
+-- Examples:
+--
+-- >>> last []
+-- Nothing
+--
+-- >>> last [1,2,3,4]
+-- Just 4
+--
+-- >>> last ['x', 'y', 'z']
+-- Just 'z'
 last :: [a] -> Maybe a
 last = undefined
